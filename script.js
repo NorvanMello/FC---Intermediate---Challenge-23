85b9d9fc389cf9896bd4952d6bb5441d56085210
Norvan <110420594+NorvanMello@users.noreply.github.com>
2026-07-09 20:23:31 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,57 @@
-import { getTime } from "./worldTimeApi.js"
-import { getQuote } from "./quoteApi.js"
-import { renderQuote } from "./quote.js"
-import { renderTime } from "./worldTime.js"
-import { setBackgroundByTime } from "./dynamicBackground.js"
-import { renderBottom, setupBottomToggle } from "./toggleBottom.js"
+import { getTime } from "./worldTimeApi.js";
+import { getQuote } from "./quoteApi.js";
+import { renderQuote } from "./quote.js";
+import { renderTime } from "./worldTime.js";
+import { setBackgroundByTime } from "./dynamicBackground.js";
+import { renderBottom, setupBottomToggle } from "./toggleBottom.js";
+import { getIPInfo } from "./ipInfoAPI.js";
 
-const quoteTextContainer = document.querySelector(".quote-container");
 const timeContainer = document.querySelector(".time-container");
 const reloadBtn = document.querySelector(".reload-btn");
-// const infoContent = document.querySelector(".info-content")
+
+const icon = document.querySelector(".icon")
+const greetingText = document.querySelector(".greeting-text");
+    
+const time = document.querySelector(".time");
+const place = document.querySelector(".place");
 
 async function loadQuote() {
-    const quoteData = await getQuote()
-    renderQuote(quoteData);
+    try {
+        const quoteData = await getQuote()
+        renderQuote(quoteData);
+    } catch (error) {
+        console.error(error);
 
-    reloadBtn.addEventListener("click", loadQuote)
+        renderQuote({ quote: "“Opportunity Is Missed By Most People Because It Is Dressed In Overalls And Looks Like Work.”", author: "Thomas A. Edison" });
+    } 
 }
 
+reloadBtn.addEventListener("click", loadQuote)
+
 async function init() {
-    const timeData = await getTime()
+    let timeData;
+    let ipInfo;
 
-    renderTime(timeContainer, timeData)
+    try {
+        timeData = await getTime()
+        ipInfo = await getIPInfo(timeData.client_ip);
+    } catch (error) {
+        console.log(error)
 
+        timeData = {
+            datetime: new Date().toISOString(),
+            timezone: "Local time"
+        };
+
+        ipInfo = "IN YOUR CURRENT LOCATION"
+    }
+
+    
+
+    renderTime(timeData, ipInfo, { icon, greetingText, time, place });
     setBackgroundByTime(timeData);
-
-    setupBottomToggle();
     renderBottom(timeData);  
+    setupBottomToggle();
 }
 
 loadQuote();
